22feb84deb59eac50c00428f315a06d6c8e83887
slightlymadhuman-42 <daniel.chapman.300@gmail.com>
2026-03-10 12:39:35 +0000

    testing the go_home subroutine

--- robot-code/robot.py
+++ robot-code/robot.py
@@ -1,11 +1,11 @@
 from creep.machine import CreepRobot, ObjectType
 from creep.logic.basic_logic import move_forward, pick_up_box
-from creep.logic.robot_subroutines import get_floor_token
+from creep.logic.robot_subroutines import get_floor_token, go_home
 
 def main():
     creep = CreepRobot()
     creep.initialise()
-    get_floor_token(creep, ObjectType.BASE)
+    go_home(creep)
 
 if __name__ == "__main__":
     main()
