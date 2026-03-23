c5409b4bcbb3dd77317ebdcd96f9cd7548bcdab8
Саймън Аткинсън <99559662+gufunkt-tacos@users.noreply.github.com>
2026-03-23 14:04:29 +0000

    new file:   PublishCode.bat

---
+++ PublishCode.bat
@@ -0,0 +1,25 @@
+@echo off
+setlocal
+
+set "SOURCE_DIR=%~dp0\robot-code"
+
+
+set "USB_DRIVE=D:\"
+
+
+set "DEST_DIR=%USB_DRIVE%"
+
+
+if not exist "%SOURCE_DIR%" (
+    echo ERROR: Robot code folder was not found.
+    pause
+    exit /b
+)
+
+
+xcopy "%SOURCE_DIR%" "%DEST_DIR%" /E
+
+echo File copied to USB successfully.
+powershell -command "$driveEject = New-Object -ComObject Shell.Application; $driveEject.Namespace(17).ParseName('D:').InvokeVerb('Eject')"
+echo USB ejected.
+pause
--- robot-code/robot.py
+++ robot-code/robot.py
@@ -1,13 +1,12 @@
 from creep.machine import CreepRobot, ObjectType
-from creep.logic.basic_logic import move_forward, pick_up_box
-from creep.logic.robot_subroutines import get_floor_token, approach_ledge
+from creep.logic.basic_logic import *
+from creep.logic.robot_subroutines import *
 import time
 
 def main():
     creep = CreepRobot()
     creep.initialise()
-    while True:
-        approach_ledge(creep)
+    collect_box_from_ledge(creep)
 
 if __name__ == "__main__":
     main()
