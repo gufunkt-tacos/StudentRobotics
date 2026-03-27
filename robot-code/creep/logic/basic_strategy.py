3f9e8e546c73d3169bd374f0fb54d5b9da863d3b
dylanhamilton-cirencester <dylan.hamilton@cirencester.ac.uk>
2026-03-27 11:20:09 +0000

    added timer checks to see if we should go home

--- robot-code/creep/logic/basic_strategy.py
+++ robot-code/creep/logic/basic_strategy.py
@@ -6,18 +6,28 @@ def strategy_base(creep: CreepRobot):
     #look for the closest box; get it
     get_token(creep, ObjectType.BASE, "floor")
 
+    if not creep.can_continue():
+        return
+
     #go to the red box but dont get it; push out the way
     go_to_closest_token(creep, ObjectType.ACID, find_closest_token(creep, ObjectType.ACID, 0), False) # False prevents the doors from opening
 
+    if not creep.can_continue():
+        return
+
     #get the one on the ledge
     get_ledge_token(creep, ObjectType.BASE,90)
+
+    if not creep.can_continue():
+        return
 
     #if we see the next blue box, get it. otherwise, go to where it would be
     floor_token_seen = get_token(creep, ObjectType.BASE, "floor")
     if(floor_token_seen != True):
         creep.drive_speed_distance(16, 100)
 
-
+    if not creep.can_continue():
+        return
 
     #if there's time, turn round the corner and get more boxes
     #once the time reaches a threshold, go home.
